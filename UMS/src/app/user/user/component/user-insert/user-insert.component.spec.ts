ff3ced014ea872d41d6959c4d83a54da9983c076
jayshah <jay.shah_7029@nihilent.com>
2024-04-14 19:09:49 +0530

    Added Spy for test cases

--- UMS/src/app/user/user/component/user-insert/user-insert.component.spec.ts
+++ UMS/src/app/user/user/component/user-insert/user-insert.component.spec.ts
@@ -1,23 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserInsertComponent } from './user-insert.component';
+import { DataServiceService } from '../../data-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from 'express';
+import { of } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('UserInsertComponent', () => {
   let component: UserInsertComponent;
   let fixture: ComponentFixture<UserInsertComponent>;
-
+  let dataServiceSpy: jasmine.SpyObj<DataServiceService>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let routeSpy: jasmine.SpyObj<ActivatedRoute>;
   beforeEach(async () => {
+
+    dataServiceSpy = jasmine.createSpyObj('DataServiceService', ['doesEmailExist']);
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    routeSpy = jasmine.createSpyObj('ActivatedRoute', [], { queryParams: of({}) });
+
+
     await TestBed.configureTestingModule({
-      declarations: [ UserInsertComponent ]
+      declarations: [ UserInsertComponent ],
+      imports: [ReactiveFormsModule],
+      providers: [
+        { provide: DataServiceService, useValue: dataServiceSpy },
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: routeSpy }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(UserInsertComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  });  
 });
+
+
